9461b11147d422aa524d18b776f578b4301ae030
Abhinav <abhi.gupta8802@gmail.com>
2023-01-29 12:18:08 +0530

    Added start and end position color changes
    Co-authored-by: Niraj Kumar <niraj-kumar-r@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isStart, setIsStart] = useState(false);
+  const [isEnd, setIsEnd] = useState(false);
   const ROW_SIZE = 5;
   const COL_SIZE = 5;
   const grid = [];
@@ -12,12 +15,27 @@ function App() {
     grid.push(row);
   }
 
+  const handleClick = (event) => {
+    if (isStart) {
+      document.getElementById(event.target.id).style.backgroundColor = "green";
+    }
+    if (isEnd) {
+      document.getElementById(event.target.id).style.backgroundColor = "red";
+    }
+  };
+
   return (
     <div className="App">
+      <button className="start-gen" onClick={() => setIsStart(true)}>
+        Start Position
+      </button>
+      <button className="end-gen" onClick={() => setIsEnd(true)}>
+        End Position
+      </button>
       <div className="matrix">
         {grid.map((row, i) =>
           row.map((cell, j) => (
-            <div className="cell" key={cell}>
+            <div className="cell" key={cell} id={cell} onClick={handleClick}>
               {cell}
             </div>
           ))
