63ed99a9d8ce7254ea443ecf95e6ab6193d432ce
Niraj Kumar <nirajk23523@gmail.com>
2023-01-29 20:34:36 +0530

    changes
    Co-authored-by: Abhinav <Navknight@users.noreply.github.com>

--- src/Algorithms/algorithms.js
+++ src/Algorithms/algorithms.js
@@ -2,10 +2,23 @@ import React from "react";
 
 export default class Algorithms {
     bfs(array, startNode, endNode) {
+        startNode = { row: startNode[0], col: startNode[1] };
+        endNode = { row: endNode[0], col: endNode[1] };
         let queue = [];
-        let visited = {};
+        let visited = new Set();
         let distance = [];
 
         queue.push({ row: startNode[0], col: startNode[1] });
+        visited.add({ row: startNode[0], col: startNode[1] });
+        distance[startNode] = 0;
+        while (queue.length > 0) {
+            let current = queue.shift();
+            if (visited.has(current) !== false) visited.add(current);
+            distance[current] =
+                startNode.row - current.row + startNode.col - current.col;
+            if (current.row === endNode.row && current.col === endNode.col) {
+                return distance[current];
+            }
+        }
     }
 }
