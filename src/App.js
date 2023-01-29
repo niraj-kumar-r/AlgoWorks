74d0b718fca377c99a3748e7dbce081569ad3fd0
Abhinav <abhi.gupta8802@gmail.com>
2023-01-29 11:48:41 +0530

    Added Grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-// import logo from "./logo.svg"
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+  const ROW_SIZE = 5;
+  const COL_SIZE = 5;
+  const grid = [];
+  for (let i = 0; i < ROW_SIZE; i++) {
+    const row = [];
+    for (let j = 0; j < COL_SIZE; j++) {
+      row.push(i * 10 + j);
+    }
+    grid.push(row);
+  }
+
+  return (
+    <div className="App">
+      <div className="matrix">
+        {grid.map((row, i) =>
+          row.map((cell, j) => (
+            <div className="cell" key={cell}>
+              {cell}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
